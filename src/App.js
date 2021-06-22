3b82f9c93f8d906890071303b2a62bbb1b121424
fayza <dev4women@gmail.com>
2021-06-22 17:28:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import './style.css';
+import imageInSrc from './imageInSrc.jpg'
+
+
+function App() {
+  return (
+
+<div>
+  
+  <div style={{border:"solid 1px black", width:100}}>
+
+<h1 className='title-red'>Your name here</h1>
+
+<div > <br/> </div>
+
+<img src={imageInSrc} alt='imageInSrc'/>
+
+<div > <br/> </div>
+
+<img src="/imageInPublic.jpg" alt="imageInPublic"/>
+
+</div>
+
+<video style={{width:320, height:240}}></video>
+<source src="./myVideo.mp4" type="video/mp4"/>
+
+</div>
+
+  );
+  }
+
+
+export default App;
